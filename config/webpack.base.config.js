05137e75fc6b1b5814af1262f07b58597791aef1
Showrin <showrinece15@gmail.com>
2020-05-26 19:36:03 +0600

    Change publicPath

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "../dist"),
     filename: "app.[contenthash:8].js",
-    publicPath: "/",
+    publicPath: "./",
   },
   module: {
     rules: [
