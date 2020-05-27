d890b916912d0719ff023c0e009c4564090df7c9
Showrin <showrinece15@gmail.com>
2020-05-27 11:56:37 +0600

    Change public path for dev server

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,4 +1,11 @@
 const merge = require("webpack-merge");
 const webpackBaseConfig = require("./webpack.base.config.js");
+const path = require("path");
 
-module.exports = merge(webpackBaseConfig, {});
+module.exports = merge(webpackBaseConfig, {
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "app.[contenthash:8].js",
+    publicPath: "/",
+  },
+});
